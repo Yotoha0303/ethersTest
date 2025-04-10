e5edb5f724febd149c087bf2712bd5a02f9a1ef0
Yotoha <103411612+Yotoha0303@users.noreply.github.com>
2025-04-10 22:36:56 +0800

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,20 @@
+// 导入ethers包 ethers @5.6.9
+import { ethers } from "ethers";
 
+//默认接口(可以获取到链上信息)
+const provider = ethers.getDefaultProvider();
+
+//infura RPC接口测试
+//const provider = new ethers.providers.JsonRpcProvider("https://sepolia.infura.io/v3/YOU_KEY");
+//vitalik.eth or 0xd8da6bf26964af9d7eed9e03e53415d37aa96045
+const address = '0xd8da6bf26964af9d7eed9e03e53415d37aa96045'
+
+const main = async ()=>{
+    //查询提供状态
+    // console.log(provider);
+
+    //获取当前network中，链上的测试币数量
+    const balance = await provider.getBalance(address)
+    console.log(`\nETH Balance of ${address} --> ${ethers.utils.formatEther(balance)} ETH\n`);
+}
+main()
